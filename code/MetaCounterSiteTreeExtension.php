8665c2e5cb2a6d3f246da54c914c3d0d52a3c73a
helpfulrobot <chris@silverstripe.com>
2015-12-31 23:42:43 +1300

    Converted to PSR-2

--- code/MetaCounterSiteTreeExtension.php
+++ code/MetaCounterSiteTreeExtension.php
@@ -1,23 +1,23 @@
 <?php
 
-class MetaCounterSiteTreeExtension extends SiteTreeExtension {
-	
-	private static $meta_title_length = 55;
-	private static $meta_description_length = 160;
+class MetaCounterSiteTreeExtension extends SiteTreeExtension
+{
+    
+    private static $meta_title_length = 55;
+    private static $meta_description_length = 160;
 
-	public function updateCMSFields(FieldList $fields) {
-		
-		$field = $fields->dataFieldByName('MetaTitle');
-		if ($field) {
-			$field->setAttribute('data-length', self::$meta_title_length);
-		}
-		
-		$field = $fields->dataFieldByName('MetaDescription');
-		if ($field) {
-			$field->setAttribute('data-length', self::$meta_description_length);
-		}
-		
-		return $fields;
-	}
-
+    public function updateCMSFields(FieldList $fields)
+    {
+        $field = $fields->dataFieldByName('MetaTitle');
+        if ($field) {
+            $field->setAttribute('data-length', self::$meta_title_length);
+        }
+        
+        $field = $fields->dataFieldByName('MetaDescription');
+        if ($field) {
+            $field->setAttribute('data-length', self::$meta_description_length);
+        }
+        
+        return $fields;
+    }
 }
