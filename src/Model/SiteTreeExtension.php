adc0584365caaea1e9d389f01b5c7059028d1a5c
Luke Fromhold <luke@fromhold.io>
2018-02-23 17:35:16 +1100

    Core class upgrade

--- src/Model/SiteTreeExtension.php
+++ src/Model/SiteTreeExtension.php
@@ -1,14 +1,18 @@
 <?php
 
-class MetaCounterSiteTreeExtension extends SiteTreeExtension
-{
-    
+namespace Innoweb\MetaCounter\Model;
+
+use SilverStripe\Core\Config\Config;
+use SilverStripe\Forms\FieldList;
+
+class SiteTreeExtension extends \SilverStripe\CMS\Model\SiteTreeExtension {
+
     public function updateCMSFields(FieldList $fields)
     {
         $field = $fields->dataFieldByName('MetaTitle');
         if ($field) {
-            $length = Config::inst()->get('MetaCounterSiteTreeExtension', 'meta_title_length');
-            $lengthExtended = Config::inst()->get('MetaCounterSiteTreeExtension', 'meta_title_length_extended');
+            $length = Config::inst()->get(SiteTreeExtension::class, 'meta_title_length');
+            $lengthExtended = Config::inst()->get(SiteTreeExtension::class, 'meta_title_length_extended');
             if ($length && $length > 0) {
                 $field->setAttribute('data-length', $length);
                 if ($lengthExtended && $lengthExtended > 0 && $lengthExtended != $length) {
@@ -16,11 +20,11 @@ class MetaCounterSiteTreeExtension extends SiteTreeExtension
                 }
             }
         }
-        
+
         $field = $fields->dataFieldByName('MetaDescription');
         if ($field) {
-            $length = Config::inst()->get('MetaCounterSiteTreeExtension', 'meta_description_length');
-            $lengthExtended = Config::inst()->get('MetaCounterSiteTreeExtension', 'meta_description_length_extended');
+            $length = Config::inst()->get(SiteTreeExtension::class, 'meta_description_length');
+            $lengthExtended = Config::inst()->get(SiteTreeExtension::class, 'meta_description_length_extended');
             if ($length && $length > 0) {
                 $field->setAttribute('data-length', $length);
                 if ($lengthExtended && $lengthExtended > 0 && $lengthExtended != $length) {
@@ -28,7 +32,7 @@ class MetaCounterSiteTreeExtension extends SiteTreeExtension
                 }
             }
         }
-        
+
         return $fields;
     }
 }
